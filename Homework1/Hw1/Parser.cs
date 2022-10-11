4398b8b0a3547aa403a45c857327dfa06bd769cf
H0TC00LER <yakov-mamin@mail.ru>
2022-10-11 19:42:52 +0300

    hw1 done

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -7,13 +7,36 @@ public static class Parser
         out CalculatorOperation operation, 
         out double val2)
     {
-        throw new NotImplementedException();
+        if (!IsArgLengthSupported(args))
+            throw new ArgumentException();
+
+        bool isVal1Correct = double.TryParse(args[0], out val1);
+        if (!isVal1Correct)
+            throw new ArgumentException();
+
+        bool isVal2Correct = double.TryParse(args[2], out val2);
+        if (!isVal2Correct)
+            throw new ArgumentException();
+
+        operation = ParseOperation(args[1]);
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        switch (arg)
+        {
+            case "+":
+                return CalculatorOperation.Plus;
+            case "-":
+                return CalculatorOperation.Minus;
+            case "*":
+                return CalculatorOperation.Multiply;
+            case "/":
+                return CalculatorOperation.Divide;
+            default:
+                throw new InvalidOperationException();
+        }
     }
 }
