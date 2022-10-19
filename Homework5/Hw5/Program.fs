a5c14887e5e798ae8b08ab555adafb39c7c399be
H0TC00LER <yakov-mamin@mail.ru>
2022-10-19 09:37:24 +0300

    hw5 done

--- Homework5/Hw5/Program.fs
+++ Homework5/Hw5/Program.fs
@@ -1,3 +1,12 @@
-﻿open System
+﻿open Hw5.Calculator
+open Hw5.Parser
+open Hw5
 
-(NotImplementedException() |> raise)       
+let Main val1 operation val2 = 
+    let args = [|val1; operation; val2|]
+    let parserResult = parseCalcArguments args
+    match parserResult with
+    | Ok resultOk ->
+        let a, b, c = resultOk
+        printfn "%A" (calculate a b c)
+    | Error error -> printfn "%A" error
