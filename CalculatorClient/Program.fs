b5994cccf2e92f56b1a85a52fc0aaf0ad415d7c1
H0TC00LER <yakov-mamin@mail.ru>
2022-10-29 10:37:39 +0300

    hw6 done

---
+++ CalculatorClient/Program.fs
@@ -0,0 +1,33 @@
+﻿module CalculatorClient
+open System
+open System.Net.Http
+open System.Net
+
+let matchOperation operation
+    = match operation with
+    | "+" -> "Plus"
+    | "-" -> "Minus"
+    | "*" -> "Multiply"
+    | "/" -> "Divide"
+    | _ -> operation
+
+let calculateAsync (client: HttpClient) = 
+    async{
+        while true do
+            let args = Console.ReadLine().Split()
+            let operation = matchOperation args[1]
+            let url = Uri($"https://localhost:59659/calculate?value1={args[0]}&operation={operation}&value2={args[2]}")
+            let! response = client.GetAsync(url) |> Async.AwaitTask
+            let! result = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+            printfn "%A" result
+    }
+    
+[<EntryPoint>]
+let main _ = 
+    use client = new HttpClient()
+    printfn "%A" (System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners())
+    Async.RunSynchronously(calculateAsync client)
+    0
+
+    
+
