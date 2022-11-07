1c8e4c63c435b8a412312af3de66ca471e1b6d5c
H0TC00LER <yakov-mamin@mail.ru>
2022-11-07 17:10:07 +0300

    hw8

--- Homework8/Hw8/Controllers/CalculatorController.cs
+++ Homework8/Hw8/Controllers/CalculatorController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using Hw8.Calculator;
 using Microsoft.AspNetCore.Mvc;
+using static System.Double;
 
 namespace Hw8.Controllers;
 
@@ -12,7 +13,27 @@ public class CalculatorController : Controller
         string operation,
         string val2)
     {
-        throw new NotImplementedException();
+        if (TryParse(val1, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out double parsedVal1) &&
+            TryParse(val2, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out double parsedVal2))
+        {
+            switch (operation)
+            {
+                case "Plus":
+                    return calculator.Plus(parsedVal1, parsedVal2);
+                case "Minus":
+                    return calculator.Minus(parsedVal1, parsedVal2);
+                case "Multiply":
+                    return calculator.Multiply(parsedVal1, parsedVal2);
+                case "Divide":
+                    if (parsedVal2 == 0)
+                        return Content(Messages.DivisionByZeroMessage);
+                    return calculator.Divide(parsedVal1, parsedVal2);
+                default:
+                    return Content(Messages.InvalidOperationMessage);
+            }
+        }
+
+        return Content(Messages.InvalidNumberMessage);
     }
     
     [ExcludeFromCodeCoverage]
--- Homework8/Hw8/Program.cs
+++ Homework8/Hw8/Program.cs
@@ -11,6 +11,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllersWithViews();
+        builder.Services.AddTransient<ICalculator, Calculator.Calculator>();
 
         var app = builder.Build();
 
