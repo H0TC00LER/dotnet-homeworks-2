c32aaa336a9fdc0cea701c1bcf946b4e05ed1674
H0TC00LER <yakov-mamin@mail.ru>
2022-11-17 02:01:35 +0300

    hw9

--- Homework9/Hw9/Services/MathCalculator/MathCalculatorService.cs
+++ Homework9/Hw9/Services/MathCalculator/MathCalculatorService.cs
@@ -1,4 +1,8 @@
 using Hw9.Dto;
+using Hw9.ErrorMessages;
+using Hw9.Parser;
+using System.Buffers;
+using System.Linq.Expressions;
 
 namespace Hw9.Services.MathCalculator;
 
@@ -6,6 +10,17 @@ public class MathCalculatorService : IMathCalculatorService
 {
     public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var parsedExpression = Parser.Parser.Parse(expression);
+            var expressionTree = ExpressionBuilder.BuildExpressionTree(parsedExpression);
+            var visitor = new Visitor();
+            var result = await visitor.Visit(expressionTree);
+            return new CalculationMathExpressionResultDto(result);
+        }
+        catch(Exception ex)
+        {
+            return new CalculationMathExpressionResultDto(ex.Message);
+        }
     }
 }
