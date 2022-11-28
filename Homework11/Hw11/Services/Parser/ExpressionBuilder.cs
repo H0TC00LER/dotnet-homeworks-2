0d55eab3b458e93cfed126d57d053d0fcb8dca99
H0TC00LER <yakov-mamin@mail.ru>
2022-11-28 13:11:43 +0300

    hw11

---
+++ Homework11/Hw11/Services/Parser/ExpressionBuilder.cs
@@ -0,0 +1,47 @@
+﻿using Hw11.ErrorMessages;
+using System.Linq.Expressions;
+
+namespace Hw11.Services.Parser
+{
+    public class ExpressionBuilder
+    {
+        static public Expression BuildExpressionTree(Queue<string> queue)
+        {
+            var treeStack = new Stack<Expression>();
+            while (queue.Any())
+            {
+                var currentNode = queue.Dequeue();
+                if (Double.TryParse(currentNode, out double number))
+                {
+                    treeStack.Push(Expression.Constant(number));
+                }
+                else
+                {
+                    var arg2 = treeStack.Pop();
+                    var arg1 = treeStack.Pop();
+                    treeStack.Push(BuildMathExpression(arg1, arg2, currentNode));
+                }
+            }
+            return treeStack.Pop();
+        }
+
+        static public Expression BuildMathExpression(Expression arg1, Expression arg2, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return Expression.Add(arg1, arg2);
+                case "-":
+                    return Expression.Subtract(arg1, arg2);
+                case "*":
+                    return Expression.Multiply(arg1, arg2);
+                case "/":
+                    if (arg2.Type == typeof(ConstantExpression) && Convert.ToDouble((arg2 as ConstantExpression).Value) == 0)
+                        throw new Exception(MathErrorMessager.DivisionByZero);
+                    return Expression.Divide(arg1, arg2);
+                default:
+                    throw new Exception();
+            }
+        }
+    }
+}
---
+++ Homework11/Hw11/Services/Parser/Visitor.cs
@@ -0,0 +1,38 @@
+﻿using Hw11.ErrorMessages;
+using System.Linq.Expressions;
+
+namespace Hw11.Services.Parser
+{
+    public class Visitor
+    {
+        public async Task<double> Visit(ConstantExpression node) => await Task.Run(() => Convert.ToDouble(node.Value));
+
+        public async Task<double> Visit(BinaryExpression node)
+        {
+            await Task.Delay(1000);
+
+            dynamic leftNode = node.Left;
+            dynamic rightNode = node.Right;
+
+            var tasks = await Task.WhenAll(new Task<double>[] { Visit(leftNode), this.Visit(rightNode) });
+            var leftNodeResult = tasks[0];
+            var rightNodeResult = tasks[1];
+
+            switch (node.NodeType)
+            {
+                case ExpressionType.Add:
+                    return leftNodeResult + rightNodeResult;
+                case ExpressionType.Subtract:
+                    return leftNodeResult - rightNodeResult;
+                case ExpressionType.Multiply:
+                    return leftNodeResult * rightNodeResult;
+                case ExpressionType.Divide:
+                    if (rightNodeResult == 0)
+                        throw new DivideByZeroException(MathErrorMessager.DivisionByZero);
+                    return leftNodeResult / rightNodeResult;
+                default:
+                    throw new Exception();
+            }
+        }
+    }
+}
