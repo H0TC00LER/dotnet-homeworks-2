574988f9b15f1b83fadddef12a29f0bbed99fe1e
H0TC00LER <112895111+H0TC00LER@users.noreply.github.com>
2022-12-12 19:42:57 +0300

    hw13

---
+++ Homework13/Homework10/Hw10/Services/Parser/Visitor.cs
@@ -0,0 +1,42 @@
+﻿using Hw10.ErrorMessages;
+using System.Linq.Expressions;
+
+namespace Hw10.Services.Parser
+{
+    public class Visitor : ExpressionVisitor
+    {
+        public async Task<double> Visit(Expression node)
+        {
+            var nodeAsBinaryExpression = node as BinaryExpression;
+
+            if (nodeAsBinaryExpression == null)
+            {
+                var nodeAsConstantExpression = node as ConstantExpression;
+                if (nodeAsConstantExpression != null)
+                    return Convert.ToDouble(nodeAsConstantExpression.Value);
+            }
+
+            await Task.Delay(1000);
+
+            var tasks = await Task.WhenAll(new Task<double>[] { Visit(nodeAsBinaryExpression.Left), this.Visit(nodeAsBinaryExpression.Right) });
+            var leftNode = tasks[0];
+            var rightNode = tasks[1];
+
+            switch (node.NodeType)
+            {
+                case ExpressionType.Add:
+                    return leftNode + rightNode;
+                case ExpressionType.Subtract:
+                    return leftNode - rightNode;
+                case ExpressionType.Multiply:
+                    return leftNode * rightNode;
+                case ExpressionType.Divide:
+                    if (rightNode == 0)
+                        throw new Exception(MathErrorMessager.DivisionByZero);
+                    return leftNode / rightNode;
+                default:
+                    throw new Exception();
+            }
+        }
+    }
+}
